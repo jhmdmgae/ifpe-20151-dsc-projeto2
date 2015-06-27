8424c4cb8a48216b26e179cc3fb9552d2602d754
Aluno <Aluno@192.168.97.242>
2015-06-27 09:40:15 -0300

    Pacotes 2

--- ifpe-20151-dsc-projeto2/src/main/java/service/AlunoDao.java
+++ ifpe-20151-dsc-projeto2/src/main/java/service/AlunoDao.java
@@ -3,7 +3,17 @@ package service;
 import jpa.Aluno;
 import dao.Dao;
 import java.util.List;
+import javax.ejb.LocalBean;
+import javax.ejb.Stateless;
+import javax.ejb.TransactionAttribute;
+import javax.ejb.TransactionAttributeType;
+import javax.ejb.TransactionManagement;
+import javax.ejb.TransactionManagementType;
 
+@Stateless
+@LocalBean
+@TransactionManagement(TransactionManagementType.CONTAINER)
+@TransactionAttribute(TransactionAttributeType.SUPPORTS)
 public class AlunoDao extends Dao {
 
     public void salvar(Aluno aluno) {
